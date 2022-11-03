a0aa39355992a57515aafb2e11b514a9639281bd
Juninho Luby <joao.domingues@luby.software>
2022-11-03 18:28:02 -0300

    WIP: Response newsletter GetAll is passing

--- JoaoLuizDeveloper.IntegrationTest/Config/ConfigApp.cs
+++ JoaoLuizDeveloper.IntegrationTest/Config/ConfigApp.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace JoaoLuizDeveloper.IntegrationTest.Config
 {
-    public class ConfigApp<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    public class ConfigApp<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseStartup<TProgram>();
+            builder.UseStartup<TStartup>();
             builder.UseEnvironment("Testing");
         }
     }
--- JoaoLuizDeveloper.IntegrationTest/Config/IntegrationTestsFixture.cs
+++ JoaoLuizDeveloper.IntegrationTest/Config/IntegrationTestsFixture.cs
@@ -1,18 +1,39 @@
-﻿using Xunit;
+﻿using JoaoLuizDeveloper.WebAPI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
 
 namespace JoaoLuizDeveloper.IntegrationTest.Config
 {
-    //[CollectionDefinition(nameof(IntegrationTestFixtureCollection))]
-    //public class IntegrationTestFixtureCollection : ICollectionFixture<IntegrationTestsFixture<ProgramTesting>>
-    //{
-          
-    //}
-
-    public class IntegrationTestsFixture<TProgram> : IDisposable where TProgram : class
+    [CollectionDefinition(nameof(IntegrationTestFixtureCollection))]
+    public class IntegrationTestFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupTests>>
     {
+
+    }
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly ConfigApp<TStartup> Factory;
+        public HttpClient client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+                //AllowAutoRedirect = true,
+                //BaseAddress = new Uri("http://localhost"),
+                //HandleCookies = true,
+                //MaxAutomaticRedirections = 7
+            };
+
+            Factory = new ConfigApp<TStartup>();
+            client = Factory.CreateClient(clientOptions);
+        }
+
         public void Dispose()
         {
-
+            client.Dispose();
+            Factory.Dispose();
         }
     }
 }
